f2980cd11e5483ae02a5b2de99c98b35fd0d55f4
Felix Lindemann <felixlindemann@web.de>
2023-01-03 23:16:59 +0100

    Update environment.prod.ts

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -1,3 +1,34 @@
 export const environment = {
   production: true
+  , lang : [  "en" , "de",    "tlh",    "fr",    "nl", "es"   ]
+  , deepMl:{
+    url: "https://api-free.deepl.com/v2/",
+    key: "df2d66e0-e7f1-6381-0142-be63a0bc77fc:fx"
+  }
+  , chuck: {
+    random: "https://api.chucknorris.io/jokes/random"
+  },
+  mathjaxConfig: {
+    "config": {
+      "loader": {
+        "load": ["output/svg", "[tex]/require", "[tex]/ams" ]
+      },
+      "tex": {
+        "inlineMath": [['$', '$'], ['\\(', '\\)']],
+        "extensions": ["AMSmath.js", "AMSsymbols.js"],
+        "packages": ["base", "require","displaymath", "ams","amsmath"]
+      },
+      "svg": { "fontCache": "global" }
+    },
+    "src": "https://cdn.jsdelivr.net/npm/mathjax@3.2.2/es5/startup.js"
+  }
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
